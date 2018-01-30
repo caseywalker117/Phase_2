3d8dc9b7282c61f0eedcc91a9b36b6691d48704b
Casey Walker <caseywalker117@gmail.com>
2018-01-29 17:58:41 -0700

    Adding the Berry Gui
    Pyqt 5 install on mac may be difficult. I am not sure how it is
    different.

--- Blob_detector/bright_spot.py
+++ Blob_detector/bright_spot.py
@@ -1,12 +1,19 @@
 import cv2
+from skimage.measure import compare.ssim
+import imutils
 import numpy as np
 
-image = cv2.imread("berry_with_light.jpg")
-orig = image.copy()
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image1 = cv2.imread("berry_without_light.jpg")
+image2 = cv2.imread("berry_with_light.jpg")
 
-(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-cv2.circle(image, maxLoc, 5, (255,0,0), 2)
 
-cv2.imshow("bright", image)
-cv2.waitKey(0)
+grayed_image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+grayed_image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
+(score,diff) = compare_ssim(grayed_image1, grayed_image2, full=True)
+
+
+
+
+
+cv2.imwrite("differece_between_images.jpg", difference_between_images)
--- Blob_detector/detect_berries.py
+++ Blob_detector/detect_berries.py
@@ -12,59 +12,46 @@ print("Detecting berries....")
 
 berry_image_gray = cv2.cvtColor(berry_image, cv2.COLOR_BGR2GRAY)
 
-###############
-#cv2.imshow("berry_image_gray", berry_image_gray)
-#cv2.waitKey(0)
-###############
-
 # blurrs the image so that you can get cleaner points to work with.
 berry_image_blurred = cv2.GaussianBlur(berry_image, (7,7), 0)
 
-#cv2.imshow("berry_image_gray", berry_image_blurred)
-#cv2.waitKey(0)
-
 berry_image_hsv = cv2.cvtColor(berry_image_blurred, cv2.COLOR_BGR2HSV)
 
-
-
-#cv2.imshow("berry_image_gray", berry_image_hsv)
-#cv2.waitKey(0)
-
-#parameters for the mask.
+#parameters for the mask, filter out black objects from white objects to detect the berries.
+#simplist way to do this against a black background.
 def hsv_color_params(color):
     lower_hue = []
     upper_hue = []
     if color == 'black':
         lower_hue = np.array([0,0,0])
-        upper_hue = np.array([255,200,80])
+        upper_hue = np.array([255,100,85])
     elif color == 'white':
         lower_hue = np.array([0,0,0])
         upper_hue = np.array([0,0,255])
     return lower_hue, upper_hue
 
+def compare(image1,image2):
+    original = image1
+    light = image2
+
+    difference_between_images = cv2.subtract(image1,image2,)
+    return difference_between_images
+
+
 chosen_color = 'black'
 lower_hue,upper_hue = hsv_color_params(chosen_color)
 
-
 black_mask = cv2.inRange(berry_image_hsv, lower_hue, upper_hue)
-
-
-
 
 # initialized the detector with no parameters
 detector_params = cv2.SimpleBlobDetector_Params()
 detector = cv2.SimpleBlobDetector_create(detector_params)
 
-
-# detect the blobs in the image.
-
-#berries = detector.detect(black_mask)
-
 berries_thresh_erode = cv2.erode(black_mask, None, iterations = 2)
-#cv2.imshow("Eroded", berries_thresh_erode)
+
 berries_thresh_dilate = cv2.dilate(berries_thresh_erode, None, iterations = 9)
 
-#berries = detector.detect(berries_thresh_dilate)
+
 _, contours, hierarchy = cv2.findContours(berries_thresh_dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 berry_id = 0
@@ -83,41 +70,14 @@ for contour in contours:
 #cv2.imshow("Berries", berries_thresh_dilate)
 
 print(berry_id," berries detected in image!")
-"""
-def draw_berry_contours(image):
-    cntrs = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntrs = cntrs[0] if imutils.is_cv2() else cntrs[1]
-    cntrs = contours.sort_contours(cntrs)[0]
 
-    for (i,c) in enumerate(cntrs):
-        (x, y , w, h) = cv2.boundingRect(c)
-        ((cX,cY)), radius = cv2.minEnclosingCircle((c), (0,0,255),2)
-        cv2.putText(image,"Berry #{}".format(i+1), (x,y-15), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.45, (0,255,0),2)
-
-
-
-draw_berry_contours(berries_thresh_dilate)
-cv2.waitKey(0)
-"""
 #detective = cv2.FastFeatureDetector_create("LIGHT SEEKER")
 #descriptor = cv2.DescriptorExtractor_create("LIGHT SEEKER")
 
-# Hue range for looking for a green led in the image
-
+# bright spot for looking for a green led in the image, will catch reflections.
 (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(berry_image_gray)
 cv2.circle(berry_image, maxLoc, 5, (255,0,0), 2)
 cv2.imshow("", berry_image)
-"""
-detect = detective.detect(berry_image)
-
-berry_image_keypoints = berry_image
-indicator_light_detected = cv2.drawKeypoints(berry_image, detect, np.array([]),(0,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv2.imshow("keypoints", indicator_light_detected)
-"""
-#berry_image = cv2.bitwise_not(berry_image)
-
-#image_with_detected_berries = cv2.drawKeypoints(berry_image, berries, np.array([]), (255,0 ,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.drawContours(berry_image,contour_list, -1,(0,255,0),2)
 # show the points that are detected
